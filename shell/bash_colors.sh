5d49c0de66b79d4f423438f8bd7ca15f16c70d65
ivanbrennan <ivan.brennan@gmail.com>
2017-10-05 20:22:01 -0400

    initial commit
    A very small subset of my dotfiles to get started on NixOS.

---
+++ shell/bash_colors.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+{
+  DULL=0
+  BRIGHT=1
+  {
+    FG_NULL=00
+    FG_BLACK=30
+    FG_RED=31
+    FG_GREEN=32
+    FG_YELLOW=33
+    FG_BLUE=34
+    FG_VIOLET=35
+    FG_CYAN=36
+    FG_WHITE=37
+  }
+  {
+    BG_NULL=00
+    BG_BLACK=40
+    BG_RED=41
+    BG_GREEN=42
+    BG_YELLOW=43
+    BG_BLUE=44
+    BG_VIOLET=45
+    BG_CYAN=46
+    BG_WHITE=47
+  }
+}
+
+# ANSI Escape Commands
+{
+  NORMAL="\033[m"
+  RESET="\033[${DULL};${FG_WHITE};${BG_NULL}m"
+  {
+    BLACK="\033[${DULL};${FG_BLACK}m"
+    RED="\033[${DULL};${FG_RED}m"
+    GREEN="\033[${DULL};${FG_GREEN}m"
+    YELLOW="\033[${DULL};${FG_YELLOW}m"
+    BLUE="\033[${DULL};${FG_BLUE}m"
+    VIOLET="\033[${DULL};${FG_VIOLET}m"
+    CYAN="\033[${DULL};${FG_CYAN}m"
+    WHITE="\033[${DULL};${FG_WHITE}m"
+  }
+  {
+    BRIGHT_BLACK="\033[${BRIGHT};${FG_BLACK}m"
+    BRIGHT_RED="\033[${BRIGHT};${FG_RED}m"
+    BRIGHT_GREEN="\033[${BRIGHT};${FG_GREEN}m"
+    BRIGHT_YELLOW="\033[${BRIGHT};${FG_YELLOW}m"
+    BRIGHT_BLUE="\033[${BRIGHT};${FG_BLUE}m"
+    BRIGHT_VIOLET="\033[${BRIGHT};${FG_VIOLET}m"
+    BRIGHT_CYAN="\033[${BRIGHT};${FG_CYAN}m"
+    BRIGHT_WHITE="\033[${BRIGHT};${FG_WHITE}m"
+  }
+}
+
---
+++ shell/make-symlinks.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -eu
+
+repo=$DOTFILES
+
+shell_files=(
+  "$repo/shell/bash_aliases"
+  "$repo/shell/bash_colors.sh"
+  "$repo/shell/bashrc"
+)
+
+main() {
+  for src in "${shell_files[@]}"; do
+    link="$HOME/.$(basename "$src")"
+
+    if already_exists "$link"; then
+      backup="${link}-backup-$(date +%s)"
+      echo "$link already exists. Backing up to $backup"
+      mv -i "$link" "$backup"
+    fi
+
+    make_symlink "$src" "$link"
+  done
+}
+
+already_exists() {
+  # non-empty && not a symlink
+  [ -s "$1" ] && [ ! -L "$1" ]
+}
+
+make_symlink() {
+  local src=$1 link=$2
+  ln -svnf "$src" "$link" | grep -Fe '->'
+}
+
+main
